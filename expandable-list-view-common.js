a7a483249e6ac0db3e8d239e12646e92215a03a4
Ricardo Bocchi <ricardo@mobilemind.com.br>
2016-01-28 11:26:50 -0200

    publication

---
+++ expandable-list-view-common.js
@@ -0,0 +1,247 @@
+var observable = require("data/observable");
+var observableArray = require("data/observable-array");
+var view = require("ui/core/view");
+var proxy = require("ui/core/proxy");
+var dependencyObservable = require("ui/core/dependency-observable");
+var builder = require("ui/builder");
+var label = require("ui/label");
+var color = require("color");
+var weakEvents = require("ui/core/weak-event-listener");
+var ITEMS = "items";
+var ITEMHEADERTEMPLATE = "itemHeaderTemplate";
+var ITEMCHILDTEMPLATE = "itemChildTemplate";
+var ISSCROLLING = "isScrolling";
+var LISTVIEW = "ExpandableListView";
+var SEPARATORCOLOR = "separatorColor";
+var ROWHEIGHT = "rowHeight";
+var knownTemplates;
+
+(function (knownTemplates) {
+    knownTemplates.itemHeaderTemplate = "itemHeaderTemplate";
+    knownTemplates.itemChildTemplate = "itemChildTemplate";
+})(knownTemplates = exports.knownTemplates || (exports.knownTemplates = {}));
+
+function onItemsPropertyChanged(data) {
+    var listView = data.object;
+    listView._onItemsPropertyChanged(data);
+}
+
+function onItemHeaderTemplatePropertyChanged(data) {
+    var listView = data.object;
+    listView.refresh();
+}
+
+function onItemChildTemplatePropertyChanged(data) {
+    var listView = data.object;
+    listView.refresh();
+}
+
+function onRowHeightPropertyChanged(data) {
+    var listView = data.object;
+    listView._onRowHeightPropertyChanged(data);
+}
+
+var ExpandableListView = (function (_super) {
+    __extends(ExpandableListView, _super);
+    function ExpandableListView() {
+        _super.apply(this, arguments);
+    }
+    Object.defineProperty(ExpandableListView.prototype, "items", {
+        get: function () {
+            return this._getValue(ExpandableListView.itemsProperty);
+        },
+        set: function (value) {            
+            this._setValue(ExpandableListView.itemsProperty, value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ExpandableListView.prototype, "itemHeaderTemplate", {
+        get: function () {
+            return this._getValue(ExpandableListView.itemHeaderTemplateProperty);
+        },
+        set: function (value) {
+            this._setValue(ExpandableListView.itemHeaderTemplateProperty, value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ExpandableListView.prototype, "itemChildTemplate", {
+        get: function () {
+            return this._getValue(ExpandableListView.itemChildTemplateProperty);
+        },
+        set: function (value) {
+            this._setValue(ExpandableListView.itemChildTemplateProperty, value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ExpandableListView.prototype, "isScrolling", {
+        get: function () {
+            return this._getValue(ExpandableListView.isScrollingProperty);
+        },
+        set: function (value) {
+            this._setValue(ExpandableListView.isScrollingProperty, value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ExpandableListView.prototype, "separatorColor", {
+        get: function () {
+            return this._getValue(ExpandableListView.separatorColorProperty);
+        },
+        set: function (value) {
+            this._setValue(ExpandableListView.separatorColorProperty, value instanceof color.Color ? value : new color.Color(value));
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ExpandableListView.prototype, "rowHeight", {
+        get: function () {
+            return this._getValue(ExpandableListView.rowHeightProperty);
+        },
+        set: function (value) {
+            this._setValue(ExpandableListView.rowHeightProperty, value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ExpandableListView.prototype.refresh = function () {
+    };
+    ExpandableListView.prototype.scrollToIndex = function (index) {
+    };
+    ExpandableListView.prototype._getItemHeaderTemplateContent = function (index) {
+        var v;
+        if (this.itemHeaderTemplate && this.items) {
+            v = builder.parse(this.itemHeaderTemplate, this);
+        }
+        return v;
+    };
+    ExpandableListView.prototype._getItemChildTemplateContent = function (index) {
+        var v;
+        if (this.itemChildTemplate && this.items) {
+            v = builder.parse(this.itemChildTemplate, this);
+        }
+        return v;
+    };
+    ExpandableListView.prototype._prepareItemHeader = function (item, index) {
+        if (item) {
+            var dataItem = this._getDataItemHeader(index);
+            if (!(dataItem instanceof observable.Observable)) {
+                item.bindingContext = null;
+            }
+            item.bindingContext = dataItem;
+            item._inheritProperties(this);
+        }
+    };
+    ExpandableListView.prototype._prepareItemChild = function (item, headerIndex, index) {
+        if (item) {
+            var dataItem = this._getDataItemChild(headerIndex, index);
+            if (!(dataItem instanceof observable.Observable)) {
+                item.bindingContext = null;
+            }
+            item.bindingContext = dataItem;
+            item._inheritProperties(this);
+        }
+    };
+    ExpandableListView.prototype._getDataItemHeader = function (index) {
+        return this.items ? this.items[index] : undefined
+    };
+    ExpandableListView.prototype._getDataItemChild = function (headerIndex, index) {
+        return this.items ? this.items[headerIndex].getItem(index) : undefined
+    };
+    ExpandableListView.prototype._getDefaultItemHeaderContent = function (index) {
+        var lbl = new label.Label();
+        lbl.bind({
+            targetProperty: "text",
+            sourceProperty: "$value"
+        });
+        return lbl;
+    };
+    ExpandableListView.prototype._getDefaultItemChildContent = function (index) {
+        var lbl = new label.Label();
+        lbl.bind({
+            targetProperty: "text",
+            sourceProperty: "$value"
+        });
+        return lbl;
+    };
+    ExpandableListView.prototype._onItemsPropertyChanged = function (data) {
+        console.log("## _onItemsPropertyChanged")
+        if (data.oldValue instanceof observable.Observable) {
+            weakEvents.removeWeakEventListener(data.oldValue, observableArray.ObservableArray.changeEvent, this._onItemsChanged, this);
+        }
+        if (data.newValue instanceof observable.Observable) {
+            weakEvents.addWeakEventListener(data.newValue, observableArray.ObservableArray.changeEvent, this._onItemsChanged, this);
+        }
+        this.refresh();
+    };
+    ExpandableListView.prototype._onItemsChanged = function (args) {
+        this.refresh();
+    };
+    ExpandableListView.prototype._onRowHeightPropertyChanged = function (data) {
+        this.refresh();
+    };
+    ExpandableListView.prototype._propagateInheritableProperties = function (view) {
+    };
+    
+    ExpandableListView.itemLoadingEvent = "itemLoading";
+    ExpandableListView.itemTapEvent = "itemTap";
+    ExpandableListView.loadMoreItemsEvent = "loadMoreItems";
+    ExpandableListView.separatorColorProperty = new dependencyObservable.Property(SEPARATORCOLOR, LISTVIEW, new proxy.PropertyMetadata(undefined));
+    ExpandableListView.itemsProperty = new dependencyObservable.Property(ITEMS, LISTVIEW, new proxy.PropertyMetadata(undefined, dependencyObservable.PropertyMetadataSettings.AffectsLayout, onItemsPropertyChanged));    
+    ExpandableListView.itemHeaderTemplateProperty = new dependencyObservable.Property(ITEMHEADERTEMPLATE, LISTVIEW, new proxy.PropertyMetadata(undefined, dependencyObservable.PropertyMetadataSettings.AffectsLayout, onItemHeaderTemplatePropertyChanged));
+    ExpandableListView.itemChildTemplateProperty = new dependencyObservable.Property(ITEMCHILDTEMPLATE, LISTVIEW, new proxy.PropertyMetadata(undefined, dependencyObservable.PropertyMetadataSettings.AffectsLayout, onItemChildTemplatePropertyChanged));
+    ExpandableListView.isScrollingProperty = new dependencyObservable.Property(ISSCROLLING, LISTVIEW, new proxy.PropertyMetadata(false, dependencyObservable.PropertyMetadataSettings.None));
+    ExpandableListView.rowHeightProperty = new dependencyObservable.Property(ROWHEIGHT, LISTVIEW, new proxy.PropertyMetadata(-1, dependencyObservable.PropertyMetadataSettings.AffectsLayout, onRowHeightPropertyChanged));
+    return ExpandableListView;
+
+})(view.View);
+
+exports.ExpandableListView = ExpandableListView;
+
+
+var ExpandableListItem = function (args) {    
+
+    // listDataHeader: header titles
+    // listDataChild: child data in format of header title, child title
+           
+    this.id = args.id
+    this.title = args.title
+    this.items = args.items || []
+    this.tag = args.tag    
+
+    ExpandableListItem.prototype.getItems = function(){
+        return this.items
+    }
+
+    ExpandableListItem.prototype.getItemsCount = function(){
+        return this.items.length
+    }
+
+    ExpandableListItem.prototype.getItem = function(index){
+        return this.items[index]
+    }
+
+    ExpandableListItem.prototype.getId = function(index){
+        return this.id
+    }
+
+    ExpandableListItem.prototype.getTitle = function(index){
+        return this.title
+    }
+
+    ExpandableListItem.prototype.addItem = function(item){
+        this.items.push(item)
+    }
+
+    ExpandableListItem.prototype.makeAndAddItem = function(args){        
+        this.addItem(new ExpandableListItem(args))
+    }
+
+    ExpandableListItem.prototype.getTag = function(args){
+        return this.tag
+    }
+}
+
+exports.ExpandableListItem = ExpandableListItem;
---
+++ expandable-list-view.android.js
@@ -0,0 +1,296 @@
+var common = require("./expandable-list-view-common");
+var layoutBaseModule = require("ui/layouts/layout-base");
+var stackLayout = require("ui/layouts/stack-layout");
+var color = require("color");
+var ITEMLOADING = common.ExpandableListView.itemLoadingEvent;
+var LOADMOREITEMS = common.ExpandableListView.loadMoreItemsEvent;
+var ITEMTAP = common.ExpandableListView.itemTapEvent;
+var REALIZED_INDEX = "realizedIndex";
+global.moduleMerge(common, exports);
+function onSeparatorColorPropertyChanged(data) {
+    var bar = data.object;
+    if (!bar.android) {
+        return;
+    }
+    if (data.newValue instanceof color.Color) {
+        bar.android.setDivider(new android.graphics.drawable.ColorDrawable(data.newValue.android));
+        bar.android.setDividerHeight(1);
+    }
+}
+common.ExpandableListView.separatorColorProperty.metadata.onSetNativeValue = onSeparatorColorPropertyChanged;
+var ExpandableListView = (function (_super) {
+    __extends(ExpandableListView, _super);
+    function ExpandableListView() {
+        _super.apply(this, arguments);
+        this._realizedItemsHeader = {};
+        this._realizedItemsChild = {};
+    }
+    ExpandableListView.prototype._createUI = function () {
+        this._android = new android.widget.ExpandableListView(this._context);
+        this._android.setCacheColorHint(android.graphics.Color.TRANSPARENT);
+        if (!this._androidViewId) {
+            this._androidViewId = android.view.View.generateViewId();
+        }
+        this._android.setId(this._androidViewId);
+        this.android.setAdapter(new ExpandableListViewAdapter(this));
+        var that = new WeakRef(this);
+        this.android.setOnScrollListener(new android.widget.AbsListView.OnScrollListener({
+            onScrollStateChanged: function (view, scrollState) {
+                var owner = this.owner;
+                if (!owner) {
+                    return;
+                }
+                if (scrollState === android.widget.AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+                    owner._setValue(common.ExpandableListView.isScrollingProperty, false);
+                    owner._notifyScrollIdle();
+                }
+                else {
+                    owner._setValue(common.ExpandableListView.isScrollingProperty, true);
+                }
+            },
+            onScroll: function (view, firstVisibleItem, visibleItemCount, totalItemCount) {
+                var owner = this.owner;
+                if (!owner) {
+                    return;
+                }
+                if (totalItemCount > 0 && firstVisibleItem + visibleItemCount === totalItemCount) {
+                    owner.notify({ eventName: LOADMOREITEMS, object: owner });
+                }
+            },
+            get owner() {
+                return that.get();
+            }
+        }));
+        this.android.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener({
+            onItemClick: function (parent, convertView, index, id) {
+                var owner = that.get();
+                if (owner) {
+                    owner.notify({ eventName: ITEMTAP, object: owner, index: index, view: owner._getRealizedView(convertView, index) });
+                }
+            }
+        }));
+    };
+    Object.defineProperty(ExpandableListView.prototype, "android", {
+        get: function () {
+            return this._android;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ExpandableListView.prototype.refresh = function () {
+        if (!this._android || !this._android.getAdapter()) {
+            return;
+        }
+        this.android.getAdapter().notifyDataSetChanged();
+    };
+    ExpandableListView.prototype.scrollToIndex = function (index) {
+        if (this._android) {
+            this._android.setSelection(index);
+        }
+    };
+    ExpandableListView.prototype._onDetached = function (force) {
+        _super.prototype._onDetached.call(this, force);
+        var keys = Object.keys(this._realizedItemsHeader);
+        var i;
+        var length = keys.length;
+        var view;
+        var key;
+        for (i = 0; i < length; i++) {
+            key = keys[i];
+            view = this._realizedItemsHeader[key];
+            view.parent._removeView(view);
+            delete this._realizedItemsHeader[key];
+        }
+
+        keys = Object.keys(this._realizedItemsChild);
+        length = keys.length;
+
+        for (i = 0; i < length; i++) {
+            key = keys[i];
+            view = this._realizedItemsChild[key];
+            view.parent._removeView(view);
+            delete this._realizedItemsChild[key];
+        }
+
+    };
+
+    ExpandableListView.prototype._getRealizedHeaderView = function (convertView, index) {
+        if (!convertView) {
+            return this._getItemHeaderTemplateContent(index);
+        }
+        return this._realizedItemsHeader[convertView.hashCode()];
+    };
+
+    ExpandableListView.prototype._getRealizedChildView = function (convertView, index) {
+        if (!convertView) {
+            return this._getItemChildTemplateContent(index);
+        }
+        return this._realizedItemsChild[convertView.hashCode()];
+    };
+
+    ExpandableListView.prototype._notifyScrollIdle = function () {
+        var keys = Object.keys(this._realizedItemsHeader);
+        var i;
+        var length = keys.length;
+        var view;
+        var key;
+        for (i = 0; i < length; i++) {
+            key = keys[i];
+            view = this._realizedItemsHeader[key];
+            this.notify({
+                eventName: ITEMLOADING,
+                object: this,
+                index: view[REALIZED_INDEX],
+                view: view
+            });
+        }
+
+        keys = Object.keys(this._realizedItemsChild);        
+        length = keys.length;
+        for (i = 0; i < length; i++) {
+            key = keys[i];
+            view = this._realizedItemsChild[key];
+            this.notify({
+                eventName: ITEMLOADING,
+                object: this,
+                index: view[REALIZED_INDEX],
+                view: view
+            });
+        }        
+    };
+    return ExpandableListView;
+})(common.ExpandableListView);
+
+exports.ExpandableListView = ExpandableListView;
+
+var ExpandableListViewAdapter = (function (_super) {
+    __extends(ExpandableListViewAdapter, _super);
+
+    // listDataHeader: header titles
+    // listDataChild: child data in format of header title, child title
+    function ExpandableListViewAdapter(listView) {
+        _super.call(this);
+        this._listView = listView;
+        this._context = this._listView._context        
+        return global.__native(this);
+    }
+    
+    ExpandableListViewAdapter.prototype.getChild = function(groupPosition, childPosititon) {
+        var dataHeader = this._listView.items[groupPosition];
+        var dataChild = dataHeader.getItem(childPosititon);
+        return dataChild;
+    }
+ 
+    ExpandableListViewAdapter.prototype.getChildId = function(groupPosition, childPosition) {
+        return childPosition;
+    }
+     
+    ExpandableListViewAdapter.prototype.getChildView = function(groupPosition, childPosition, isLastChild, convertView, parent) {
+                
+        if (!this._listView) {
+            return null;
+        }
+        var view = this._listView._getRealizedChildView(convertView, childPosition);
+        var args = {
+            eventName: ITEMLOADING, object: this._listView, index: childPosition, view: view,
+            android: parent,
+            ios: undefined
+        };
+        this._listView.notify(args);
+        if (!args.view) {
+            args.view = this._listView._getDefaultItemChildContent(childPosition);
+        }
+        if (args.view) {
+            if (this._listView.rowHeight > -1) {
+                args.view.height = this._listView.rowHeight;
+            }
+            else {
+                args.view.height = Number.NaN;
+            }
+            this._listView._prepareItemChild(args.view, groupPosition, childPosition);
+            if (!args.view.parent) {
+                if (args.view instanceof layoutBaseModule.LayoutBase) {
+                    this._listView._addView(args.view);
+                    convertView = args.view.android;
+                }
+                else {
+                    var sp = new stackLayout.StackLayout();
+                    sp.addChild(args.view);
+                    this._listView._addView(sp);
+                    convertView = sp.android;
+                }
+            }
+            this._listView._realizedItemsChild[convertView.hashCode()] = args.view;
+            args.view[REALIZED_INDEX] = childPosition;
+        }
+        return convertView;
+    }
+ 
+    ExpandableListViewAdapter.prototype.getChildrenCount = function(groupPosition) {
+        var dataHeader = this._listView.items[groupPosition]
+        return dataHeader.getItemsCount()
+    }
+     
+    ExpandableListViewAdapter.prototype.getGroup = function(groupPosition) {
+        return this._listView.items[groupPosition];
+    }
+ 
+    ExpandableListViewAdapter.prototype.getGroupCount = function() {        
+        return (this._listView && this._listView.items) ? this._listView.items.length : 0;
+    }
+     
+    ExpandableListViewAdapter.prototype.getGroupId = function(groupPosition) {
+        return groupPosition;
+    }
+ 
+    ExpandableListViewAdapter.prototype.getGroupView = function(groupPosition, isExpanded, convertView, parent) {
+        
+        if (!this._listView) {
+            return null;
+        }
+        var view = this._listView._getRealizedHeaderView(convertView, groupPosition);
+        var args = {
+            eventName: ITEMLOADING, object: this._listView, index: groupPosition, view: view,
+            android: parent,
+            ios: undefined
+        };
+        this._listView.notify(args);
+        if (!args.view) {
+            args.view = this._listView._getDefaultItemHeaderContent(groupPosition);
+        }
+        if (args.view) {
+            if (this._listView.rowHeight > -1) {
+                args.view.height = this._listView.rowHeight;
+            }
+            else {
+                args.view.height = Number.NaN;
+            }
+            this._listView._prepareItemHeader(args.view, groupPosition);
+            if (!args.view.parent) {
+                if (args.view instanceof layoutBaseModule.LayoutBase) {
+                    this._listView._addView(args.view);
+                    convertView = args.view.android;
+                }
+                else {
+                    var sp = new stackLayout.StackLayout();
+                    sp.addChild(args.view);
+                    this._listView._addView(sp);
+                    convertView = sp.android;
+                }
+            }
+            this._listView._realizedItemsHeader[convertView.hashCode()] = args.view;
+            args.view[REALIZED_INDEX] = groupPosition;
+        }
+        return convertView;
+    }
+ 
+    ExpandableListViewAdapter.prototype.hasStableIds = function() {
+        return false;
+    }
+     
+    ExpandableListViewAdapter.prototype.isChildSelectable = function(groupPosition, childPosition) {
+        return true;
+    }
+
+    return ExpandableListViewAdapter;
+})(android.widget.BaseExpandableListAdapter);
